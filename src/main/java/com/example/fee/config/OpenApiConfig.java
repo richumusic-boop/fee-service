a05122f6e0231944315c1eac94c7325ea89f1423
nizamudeen pa <richumusic@gmail.com>
2026-02-08 23:54:59 +0400

    Initial commit

---
+++ src/main/java/com/example/fee/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.example.fee.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fee Service API")
+                        .version("1.0"))
+                .servers(List.of(
+                        new Server()
+                                .url("https://fictional-palm-tree-v6j9v57prvqwf69jj-8081.app.github.dev")
+                ));
+    }
+}
